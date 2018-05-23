b9a572f16bcced626c822076832fddae5fda6cd8
konradkurcaba <konradkurcaba@gmail.com>
2018-05-23 20:51:57 +0200

    Added Maven support. Game logic now check whether user have to delete enemy piece

---
+++ src/main/java/pl/kurcaba/Controller.java
@@ -0,0 +1,4 @@
+package pl.kurcaba;
+
+public class Controller {
+}
---
+++ src/test/java/pl/kurcaba/GameWindowTest.java
@@ -0,0 +1,25 @@
+package pl.kurcaba;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GameWindowTest {
+
+    @Test
+
+    public void myTests() throws Exception
+    {
+
+      
+
+
+    }
+
+
+
+}
